9a1ef81fa8a2249207dbf4f537f295089a804b00
Adnyx <53366834+Adnyx@users.noreply.github.com>
2025-05-16 08:30:47 +0200

    Update backup-script.sh

--- Backups/backup-script.sh
+++ Backups/backup-script.sh
@@ -16,7 +16,9 @@ fi
 BACKUP_ROOT="/tmp/backup"
 SRC_DIRS=("/srv" "/var" "/home" "/users")
 SNAPSHOT_FILE="$BACKUP_ROOT/snapshot.snar"
-GPG_RECIPIENT="theo.dubois@std.heh.be"
+
+# MAKE SURE TO PUT THE EMAIL YOU'LL USE FOR GPG BELOW
+GPG_RECIPIENT="YOUR EMAIL"
 FULL_DAY="Sunday"
 RETENTION_DAYS=7
 DATE=$(date +%F)
